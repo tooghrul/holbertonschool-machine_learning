b0b564ca1f022c3c7d132cf0918599cd344ac9b5
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-09 15:56:40 +0400

    pandas0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import pandas as pd
+'''Importing pandas module'''
 
 def from_numpy(array):
+    '''Implementation'''
     num_cols = array.shape[1]
     col_names = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=col_names)
