3b787f8354f601f19311ff7145bacb4665b23ad7
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-09 15:52:18 +0400

    pandas0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -2,7 +2,8 @@
 import pandas as pd
 
 def from_numpy(array):
-    col_names = [chr(65+i) for i in range(26)]
+    col_count = array.shape()
+    col_names = [chr(65+i) for i in range(col_count)]
     df = pd.DataFrame(array, columns=col_names)
     return df
 
