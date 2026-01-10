ae0fff744fc8c0d16386214c52d0fea3ccc1d88e
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 00:49:57 +0400

    pandas/4,5

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 4: To Array
+"""
+import pandas as pd
+import numpy as np
+
+
+def array(df):
+    """
+    Takes the last 10 rows of the High and Close columns
+    and converts them into a numpy.ndarray.
+    Args:
+        df: pd.DataFrame
+    Returns:
+        The numpy.ndarray
+    """
+    return df[['High', 'Close']].tail(10).to_numpy()
---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 5: Slice
+"""
+import pandas as pd
+
+
+def slice(df):
+    """
+    Extracts columns High, Low, Close, Volume_(BTC) and
+    selects every 60th row.
+    Args:
+        df: pd.DataFrame
+    Returns:
+        The sliced pd.DataFrame
+    """
+    cols = ['High', 'Low', 'Close', 'Volume_(BTC)']
+    return df[cols].iloc[::60]
