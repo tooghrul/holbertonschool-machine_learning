f31e1124e8e33ddfd89a10cdb829f4da4b00cb33
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 02:01:25 +0400

    plot/1,2

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Task 1: Scatter Plot
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Plots height vs weight as a scatter plot.
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot scatter with magenta points ('m')
+    plt.scatter(x, y, c='m')
+    
+    # Set labels and title
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+    
+    plt.show()
---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Task 2: Change Scale
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """
+    Plots exponential decay of C-14 with a logarithmic y-axis.
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot data
+    plt.plot(x, y)
+    
+    # Set y-axis to logarithmic scale
+    plt.yscale('log')
+    
+    # Set labels, title, and x-axis limit
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    plt.xlim(0, 28650)
+    
+    plt.show()
