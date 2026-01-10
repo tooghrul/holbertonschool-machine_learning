1367fae3426ee5bbcc9e99a19fd637e6cddb0c38
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 02:02:37 +0400

    plot/3,4 edited

---
+++ math/plotting/3-two.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Task 3: Two Plots
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """
+    Plots two exponential decay lines (C-14 and Ra-226).
+    """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot y1 (C-14) as dashed red line
+    plt.plot(x, y1, 'r--', label='C-14')
+    
+    # Plot y2 (Ra-226) as solid green line
+    plt.plot(x, y2, 'g-', label='Ra-226')
+    
+    # Set labels, title, and limits
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    
+    # Add legend to upper right corner
+    plt.legend(loc='upper right')
+    
+    plt.show()
---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Task 4: Frequency
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Plots a histogram of student grades.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot histogram with bins every 10 units and black outline
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    
+    # Set labels and title
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.xlim(0, 100)
+    
+    plt.show()
