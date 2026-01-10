32208899013a6c114254e592bd8e40a237f244a2
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 00:57:32 +0400

    pandas/6

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 6: Flip it and Switch it
+"""
+import pandas as pd
+
+
+def flip_switch(df):
+    """
+    Sorts the data in reverse chronological order and
+    transposes the sorted dataframe.
+    Args:
+        df: pd.DataFrame
+    Returns:
+        The transformed pd.DataFrame
+    """
+    return df.sort_values('Timestamp', ascending=False).transpose()
