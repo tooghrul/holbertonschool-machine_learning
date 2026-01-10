62dc316f8dfb8c9bc429c09da34cf00b2c7d7cb2
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 00:42:46 +0400

    pandas/2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+task 2 - load from File
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    loads data from a file as a pd.DataFrame
+    args:
+        filename: file to load from
+        delimiter: column separator
+    returns:
+        The loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)i
