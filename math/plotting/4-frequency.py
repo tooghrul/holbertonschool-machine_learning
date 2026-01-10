728fc644a2e80b6638dc449cc2554d043c3352d1
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-11 02:12:41 +0400

    pandas/4 edited

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,14 +10,11 @@ def frequency():
     """
     Plots a histogram of student grades.
     """
+    #!/usr/bin/env python3
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    # Plot histogram with bins every 10 units and black outline
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-
-    # Set labels and title
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
