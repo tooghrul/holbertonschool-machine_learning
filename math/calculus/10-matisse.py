f85958e905a4ceff67d8bd706a6ebce68e969026
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-15 16:36:57 +0400

    calculus/10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,7 +3,7 @@
 def poly_derivative(poly):
     """Implementation"""
     new_poly = [0 for i in range(len(poly)-1)]
-    for i in range(len(new_poly)-1):
+    for i in range(len(new_poly)):
         new_poly[i] = (i+1)*poly[i+1]
     
     if len(poly) == 1:
