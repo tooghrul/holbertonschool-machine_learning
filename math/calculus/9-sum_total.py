705893cf1fe68801dba57c014e08ca8b6e287b5e
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-18 03:29:32 +0400

    calculus/9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def summation_i_squared(n):
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return None
 
     return (n*(n+1)*(2*n+1)) / 6
