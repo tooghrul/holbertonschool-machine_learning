39f481ae9759231a9c6d71d6a150d91b43d69b56
tooghrul <toghrulallahverdiev@gmail.com>
2026-01-18 03:37:21 +0400

    calculus/17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -2,10 +2,9 @@
 """17-integrate"""
 
 def poly_integral(poly, C=0):
-    """The implementation"""
-    if not (isinstance(poly, list) or isinstance(C, int)):
+    if not isinstance(poly, list) or not isinstance(C, int):
         return None
-    if len(poly) == 0:
+    if poly == []:
         return None
 
     result = [C]
@@ -17,8 +16,9 @@ def poly_integral(poly, C=0):
         if value.is_integer():
             value = int(value)
         result.append(value)
- 
+
     while len(result) > 1 and result[-1] == 0:
         result.pop()
 
     return result
+
