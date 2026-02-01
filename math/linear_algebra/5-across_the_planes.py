3895a7e5a9e37201bc9dc0fa02946f46762bd866
tooghrul <toghrulallahverdiev@gmail.com>
2026-02-01 14:09:56 +0400

    5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,6 +4,8 @@
 
 def add_matrices2D(mat1, mat2):
     """Task 5"""
+    if len(mat1) == [] or len(mat2) == []:
+        return None
     def matrix_shape(matrix):
         size = []
         if not isinstance(matrix, list):
@@ -12,15 +14,6 @@ def add_matrices2D(mat1, mat2):
             size.append(len(matrix))
             matrix = matrix[0]
             return size + matrix_shape(matrix)
-
-    if not isinstance(mat1, list) or not isinstance(mat2, list):
-        return None
-
-    if mat1 == [[]] and mat2 == [[]]:
-        return None
-
-    if not mat1 or not mat2:
-        return None
 
     if (matrix_shape(mat1) != matrix_shape(mat2)):
         return None
