ea96970a6aaf9d9375b98f882cba5f96afb5f8c6
tooghrul <toghrulallahverdiev@gmail.com>
2026-02-01 14:01:14 +0400

    linalg/5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -12,7 +12,7 @@ def add_matrices2D(mat1, mat2):
             size.append(len(matrix))
             matrix = matrix[0]
             return size + matrix_shape(matrix)
-    if (matrix_shape(mat1) != matrix_shape(mat2)) or (len(mat1) == 0) or (len(mat2) == 0):
+    if (matrix_shape(mat1) != matrix_shape(mat2)) or ((len(mat1) == 0) and (len(mat2) == 0)):
         return None
 
     new_matrix = [[mat1[i][j]+mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
