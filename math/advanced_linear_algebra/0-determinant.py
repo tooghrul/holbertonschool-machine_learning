d4b3739a4d2b7e854a8972cf1c75dc331d64a387
tooghrul <toghrulallahverdiev@gmail.com>
2026-02-07 12:48:15 +0400

    0-determinant.py

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Determinant"""
+
+def determinant(matrix):
+    """Implementation"""
+    def is_square(matrix):
+        """Checking if the matrix is square"""
+        if len(matrix) != len(matrix[0]):
+            raise ValueError("matrix must be a square matrix")
+
+    def is_matrix(matrix):
+        """Checking matrix type"""
+        list_of_instance = [isinstance(i, list) for i in matrix]
+        if False in list_of_instance or len(list_of_instance) == 0:
+            raise TypeError("matrix must be a list of lists")
+
+    def get_size(matrix):
+        """Getting the size of matrix"""
+        return [len(matrix), len(matrix[0])]
+
+    def def2(matrix):
+        """Calculating the determinant of 2x2 matrix"""
+        return matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[1][1]
+
+    def def3(matrix):
+        """Calculating the determinant of 3x3 matrix"""
+        det = 0
+        for i in range(3):
+            minor = [[matrix[j][k] for k in range(3) if k != i] for j in range(1,3)]
+            det += ((-1)**i) * matrix[0][i] * det2(minor)
+        return det
+
+    is_matrix(matrix)
+    is_square(matrix)
+    size = get_size(matrix)
+    if matrix is [[]]:
+        return 1
+
+    match size:
+        case [1,1]:
+            return matrix[0][0]
+        case [2,2]:
+            return det2(matrix)
+        case [3,3]:
+            return det3(matrix)
