4971a5783c338c503d0ffb8dbd4542a401306ff5
tooghrul <toghrulallahverdiev@gmail.com>
2026-02-10 04:40:49 +0400

    .

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+
 class Poisson:
-    """Represents a Poisson distribution"""
+    """
+    Represents a Poisson distribution
+    """
 
     def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes the Poisson distribution
+
+        data: list of observed data points
+        lambtha: expected number of occurrences
+        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -19,18 +28,41 @@ class Poisson:
         self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
-        """Calculates the PMF for a given number of successes"""
+        """
+        Calculates the PMF for a given number of successes
+
+        k: number of successes
+        """
         if not isinstance(k, int):
             k = int(k)
 
         if k < 0:
             return 0
 
-        # factorial(k)
+        # Calculate factorial of k
         fact = 1
         for i in range(1, k + 1):
             fact *= i
 
-        # Poisson PMF: (λ^k * e^(-λ)) / k!
+        # Euler's number approximation
         e = 2.718281828459045
+
         return (self.lambtha ** k * (e ** (-self.lambtha))) / fact
+
+    def cdf(self, k):
+        """
+        Calculates the CDF for a given number of successes
+
+        k: number of successes
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        cdf_value = 0
+        for i in range(0, k + 1):
+            cdf_value += self.pmf(i)
+
+        return cdf_value
