39d04d62c3b3bd438a28e21f0a91d50fcfa54602
tooghrul <toghrulallahverdiev@gmail.com>
2026-02-10 04:35:43 +0400

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
+
+import math
+
+
 class Poisson:
     """Represents a Poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        # Case 1: data is None → use lambtha
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
             return
 
-        # Case 2: data is given
         if not isinstance(data, list):
             raise TypeError("data must be a list")
 
         if len(data) < 2:
             raise ValueError("data must contain multiple values")
 
-        # Estimate lambtha from data (mean)
         self.lambtha = float(sum(data) / len(data))
 
+    def pmf(self, k):
+        """
+        Calculates the PMF for a given number of successes
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        # PMF formula: (λ^k * e^(-λ)) / k!
+        return (self.lambtha ** k * math.exp(-self.lambtha)) / math.factorial(k)
