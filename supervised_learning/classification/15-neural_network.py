b2abdf196ff4c24c96c78afca30490ae52127989
tooghrul <toghrulallahverdiev@gmail.com>
2026-03-28 16:25:26 +0400

    .

---
+++ supervised_learning/classification/15-neural_network.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+"""Module that defines a NeuralNetwork with one hidden layer."""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neural network with one hidden layer for binary classification."""
+
+    def __init__(self, nx, nodes):
+        """ Initialize NeuralNetwork """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for W1."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2."""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """ Forward propagation """
+        z1 = self.__W1 @ X + self.__b1
+        self.__A1 = 1 / (1 + np.e**(-z1))
+        z2 = self.__W2 @ self.__A1 + self.__b2
+        self.__A2 = 1 / (1 + np.e**(-z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """ Cost of the model (based on logistic reg) """
+        J = np.sum(
+            -Y * np.log(A) - (1 - Y) * np.log(1.0000001 - A)
+            ) / A.shape[1]
+        return J
+
+    def evaluate(self, X, Y):
+        """ evaluate NN """
+        A1, A2 = self.forward_prop(X)
+        Y_pred = np.where(A2 >= 0.5, 1, 0)
+        return Y_pred, self.cost(Y, A2)
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """ apply gradient descent algorithm """
+        m = Y.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Train neuron """
+        if not isinstance(iterations, int):
+            raise TypeError('iterations must be an integer')
+        if iterations < 1:
+            raise ValueError('iterations must be a positive integer')
+        if not isinstance(alpha, float):
+            raise TypeError('alpha must be a float')
+        if alpha <= 0:
+            raise ValueError('alpha must be positive')
+
+        if verbose or graph:
+            if not isinstance(step, int):
+                raise TypeError('step must be an integer')
+            if step <= 0 or step > iterations:
+                raise ValueError('step must be positive and <= iterations')
+        
+        costs = []
+        iters = []
+        
+        for i in range(iterations + 1):
+            if i < iterations:
+                self.forward_prop(X)
+
+            if i == iterations or i % step == 0:
+                cost = self.cost(Y, self.__A)
+                if verbose:
+                    print("Cost after {} iterations: {}".format(i, cost))
+                if graph:
+                    costs.append(cost)
+                    iters.append(i)
+
+            if i < iterations:
+                self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
+
+        if graph:
+            plt.plot(iters, costs, 'b-')
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
+            plt.show()
+
+        return self.evaluate(X, Y)
