4d2ed51c4b2bb3c75dd5dda04785e51d14800e3e
tooghrul <toghrulallahverdiev@gmail.com>
2026-04-23 17:47:38 +0400

    .

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
+"""Module for performing valid convolution on grayscale images."""
+
 import numpy as np
+
 
 def convolve_grayscale_valid(images, kernel):
     """
-    Performs a valid convolution on grayscale images
+    Performs a valid convolution on grayscale images.
 
-    images: numpy.ndarray of shape (m, h, w)
-    kernel: numpy.ndarray of shape (kh, kw)
+    Args:
+        images (numpy.ndarray): Array of shape (m, h, w) containing
+            grayscale images.
+            m: number of images
+            h: height of images
+            w: width of images
+        kernel (numpy.ndarray): Array of shape (kh, kw) containing
+            the convolution kernel.
+            kh: kernel height
+            kw: kernel width
 
-    Returns: numpy.ndarray of shape (m, h - kh + 1, w - kw + 1)
+    Returns:
+        numpy.ndarray: Convolved images of shape
+            (m, h - kh + 1, w - kw + 1)
     """
-
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Output dimensions
     output_h = h - kh + 1
     output_w = w - kw + 1
 
-    # Initialize output
     output = np.zeros((m, output_h, output_w))
 
-    # Only TWO loops: over spatial positions
     for i in range(output_h):
         for j in range(output_w):
-            # Slice all images at once (vectorized over m)
-            slice_ = images[:, i:i+kh, j:j+kw]
-
-            # Element-wise multiply and sum over kernel dims
-            output[:, i, j] = np.sum(slice_ * kernel, axis=(1, 2))
+            image_slice = images[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(image_slice * kernel,
+                                    axis=(1, 2))
 
     return output
