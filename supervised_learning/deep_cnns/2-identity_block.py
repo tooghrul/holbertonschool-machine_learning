5b8f42c573594c4dcffc31a328c7150a77bcc52c
tooghrul <toghrulallahverdiev@gmail.com>
2026-05-09 16:14:43 +0400

    .

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -8,20 +8,20 @@ from tensorflow import keras as K
 def identity_block(A_prev, filters):
     """
     Builds an identity block as described in Deep Residual Learning (2015)
-    
+
     Args:
         A_prev: output from the previous layer
         filters: tuple/list containing F11, F3, F12 (filters for conv layers)
-        
+
     Returns:
         The activated output of the identity block
     """
     # Retrieve filters
     F11, F3, F12 = filters
-    
+
     # Initialize He Normal with seed 0
     init = K.initializers.HeNormal(seed=0)
-    
+
     # --- First Component of Main Path ---
     # 1x1 convolution to reduce dimensionality
     X = K.layers.Conv2D(
@@ -32,7 +32,7 @@ def identity_block(A_prev, filters):
     )(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
-    
+
     # --- Second Component of Main Path ---
     # 3x3 convolution
     X = K.layers.Conv2D(
@@ -43,7 +43,7 @@ def identity_block(A_prev, filters):
     )(X)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
-    
+
     # --- Third Component of Main Path ---
     # 1x1 convolution to restore dimensionality
     X = K.layers.Conv2D(
@@ -53,10 +53,10 @@ def identity_block(A_prev, filters):
         kernel_initializer=init
     )(X)
     X = K.layers.BatchNormalization(axis=3)(X)
-    
+
     # --- Final Step ---
     # Add Shortcut (Identity) to the main path
     X = K.layers.Add()([X, A_prev])
     X = K.layers.Activation('relu')(X)
-    
+
     return X
